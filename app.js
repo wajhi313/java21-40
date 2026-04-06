fd2c1b730e61591830f13fb607c3272b0f7b0948
wajhi rizvi <smart.wajhee96@gmail.com>
2026-04-06 23:18:25 +0500

    java21-40

---
+++ app.js
@@ -0,0 +1,68 @@
+Chapter = 21^25
+
+// Que 1
+// let firstName = prompt("Enter the fist name");
+// let lastName = prompt("Enter the last name")
+// let fullName = firstName + " " + lastName;
+// document.writeln("Hello " + fullName + " !")
+
+// Que 2
+// let userInfo = prompt("My Favorite phone is");
+// let inputLength = userInfo.length;
+// document.writeln("My Favorite phone model is:" + " " + userInfo + "</br>" );
+// document.writeln("length of a string is " + inputLength);
+
+// Que 3
+// let cityName = "Pakistani";
+// let find = cityName.indexOf("n");
+// document.writeln("index of 'n' is " + find);
+
+// Que 4
+// let word = "Hello World"
+// let index = word.lastIndexOf("l");
+// document.writeln(index);
+
+// Que 5
+// let word = "Pakistan"
+// let update = word[3];
+// document.writeln(update);
+
+// Que 6
+// let firstName = "Wajhi";
+// let lastName = "Rizvi";
+// let fullName = firstName.concat(" " + lastName);
+// document.writeln("Hello " + fullName + " !" );
+
+// Que 7
+// let city = "Hyderabad";
+// let updateCity = city.replace("Hyderabad","Islamabad")
+// document.writeln(updateCity);
+
+// Que 8
+// let msg = "ALi and Sami are the best friends, they play cricket and football together. ;"
+// let updateMsg = msg.replace(/and/g, "&");
+// document.writeln(updateMsg);
+
+// Que 9
+// let str = "472";
+// let num = Number(str);
+
+// document.writeln("Value: " + str + "</br>");
+// document.writeln("Type: " + typeof str + "</br>");
+// document.writeln("Value: " + str + "</br>");
+// document.writeln("Type: " + typeof num);
+
+// Que 10
+// let inputInfo = prompt("What's your fav dry fruit?")
+// let update = inputInfo.toUpperCase();
+
+// document.writeln(inputInfo + "</br>");
+// document.writeln(update);
+
+// Que 11
+// let userInput = prompt("What's your language?");
+// let cap = userInput.slice(0,1).toUpperCase();
+// let nor = userInput.slice(1).toLowerCase();
+
+// console.log(userInput);
+// console.log(cap + nor);
